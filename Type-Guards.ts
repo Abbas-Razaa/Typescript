0b95b5b82fb9ad0824ca2df135c9ccac91b65ef1
Abbas Raza <abbasrazaa02@gmail.com>
2024-10-07 14:55:59 +0500

    TypeGuards

---
+++ Type-Guards.ts
@@ -0,0 +1,43 @@
+// Type Guards and Typescript utilities Types
+// Using typeof and instanceof
+// partial, required, Readonly
+
+// type guards -> type narrowing
+// function TypeGuard(arg: string | number | any) {
+//     if (typeof arg === 'number') {
+//         return 'number';
+//     } else if (typeof arg === 'string') {
+//         return 'string';
+//     } else {
+//         throw new Error('Invalid Argument');
+//     }
+// }
+
+// console.log(TypeGuard(10)); // number
+// console.log(TypeGuard('Hello')); // string
+// console.log(TypeGuard(true)); // Error
+
+
+// instanceof -> instance narrowing
+class TVremote{
+    switchTVoff() {
+        console.log('Switched off tv');
+    }
+}
+
+class CarRemote{
+    switchCaroff() {
+        console.log('Switched off car');
+    }
+}
+
+const tv = new TVremote();
+const car = new CarRemote();
+
+function switchoff(device: TVremote | CarRemote) {
+    if (device instanceof TVremote) {
+        device.switchTVoff();
+    } else {
+        device.switchCaroff();
+    }
+}
